321c5ec0ec2f0b1632ab4e0f2892aa89b7437d6e
jlyublin42 <jlyublin@gmail.com>
2021-10-26 10:22:42 -0400

    Revert "Merge branch 'main' of https://github.com/Ghost2600/Elevator-Mini-Game"
    This reverts commit 7576a758fd80ecc7bec2e98932219fbee00cafb7.

--- Assets/Script/pauseMenu.cs
+++ Assets/Script/pauseMenu.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PauseMenu : MonoBehaviour
+public class pauseMenu : MonoBehaviour
 {
     public GameObject pauseUI;
     bool isPaused = false;
@@ -14,38 +14,30 @@ public class PauseMenu : MonoBehaviour
             TogglePause();
         }
     }
-
     public void TogglePause()
     {
         if (isPaused == true)
         {
-            //unpause
+            //unpauses time
             Time.timeScale = 1.0f;
 
-            //Hides the button when the game is unpaused.
+            //hides button when unpaused
             pauseUI.SetActive(false);
 
-            //When ispaused is true, this line will set it to false
             isPaused = false;
         }
         else
         {
-            //pause
-            Time.timeScale = 0.0f;
-
-            //Shows the button when the game is paused.
+            //pauses time
+            Time.timeScale = 0;
             pauseUI.SetActive(true);
-
-            //When ispaused is false, this line will set it to true
             isPaused = true;
         }
-        //Sets ispaused to the oppisite of what it is right now
         //isPaused = !isPaused;
     }
-
-
     public void Quit()
     {
         Application.Quit();
     }
 }
+
