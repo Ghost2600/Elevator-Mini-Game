13b9a8100e6f103710bc18e4f9f3db7d6109ff25
Ghost2600 <90476678+Ghost2600@users.noreply.github.com>
2021-11-01 23:50:31 -0400

    Maybe

---
+++ Assets/Script/PlayerInteraction.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour {
+    public enum InteractionType {
+        Click,
+        Hold,
+        Minigame,
+    }
+
+    public InteractionType interactionType;
+
+    public abstract string GetDescription();
+}
